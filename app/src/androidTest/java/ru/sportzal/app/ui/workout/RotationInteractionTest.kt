f9635c69548f05d7b18c447c8ec67c92250e8ac5
bimjim225-ship-it <bimjim225@gmail.com>
2026-09-08 21:39:51 +0300

    Fix rotation test save callback signature

--- app/src/androidTest/java/ru/sportzal/app/ui/workout/RotationInteractionTest.kt
+++ app/src/androidTest/java/ru/sportzal/app/ui/workout/RotationInteractionTest.kt
@@ -43,7 +43,7 @@ class RotationInteractionTest {
                 WorkoutScreen(ui, { "0:00" }, { id, weight, reps, rir, answered ->
                     drafts[id] = SetDraft(weight, reps, rir, drafts.getValue(id).context, answered)
                     publish()
-                }, {}, { focused ->
+                }, { _, _ -> }, { focused ->
                     if (focused) coordinator.beginInteraction() else coordinator.endInteraction()
                     publish()
                 }, { publish() })
