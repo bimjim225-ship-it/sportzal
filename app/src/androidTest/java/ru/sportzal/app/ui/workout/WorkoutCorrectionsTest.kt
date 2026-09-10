ba134b9a73e69618864b40862b17bf34d9d8d101
bimjim225-ship-it <bimjim225@gmail.com>
2026-09-10 07:12:36 +0300

    Fix Compose test assertion import

--- app/src/androidTest/java/ru/sportzal/app/ui/workout/WorkoutCorrectionsTest.kt
+++ app/src/androidTest/java/ru/sportzal/app/ui/workout/WorkoutCorrectionsTest.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
