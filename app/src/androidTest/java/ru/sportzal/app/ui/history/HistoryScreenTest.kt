1bd8ec54569b02a7ae420f466b6d77eaf2a38d7b
bimjim225-ship-it <bimjim225@gmail.com>
2026-09-11 21:02:04 +0300

    test: fix HistoryScreenTest imports

--- app/src/androidTest/java/ru/sportzal/app/ui/history/HistoryScreenTest.kt
+++ app/src/androidTest/java/ru/sportzal/app/ui/history/HistoryScreenTest.kt
@@ -3,7 +3,6 @@ package ru.sportzal.app.ui.history
 import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollToNode
